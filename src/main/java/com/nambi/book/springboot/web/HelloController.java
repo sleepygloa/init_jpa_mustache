b3a8d882846c7c676e016b534d52f019824724a2
sleepygloa <sleepygloa@gmail.com>
2020-11-12 09:35:13 +0900

    내장 서버(tomcat 대안?) 추가 및 HelloController 테스트

---
+++ src/main/java/com/nambi/book/springboot/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.nambi.book.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String Hello(){
+        return "hello";
+    }
+}
