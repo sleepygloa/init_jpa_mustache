01aa861661a30559b585f76341ab7002ecd07e85
sleepygloa <sleepygloa@gmail.com>
2020-11-12 13:40:45 +0900

    1.JPA Auditing 을 이용한 입력시간, 수정시간 자동 입력

--- src/test/java/com/nambi/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/nambi/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RunWith(SpringRunner.class)
@@ -46,6 +47,29 @@ public class PostsRepositoryTest {
         assertThat(posts.getContent()).isEqualTo(content);
 
 
+    }
+    
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+        postsRepository.save(Posts.builder()
+        .title("title")
+                .content("content")
+                .author("author")
+                .build()
+        );
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>>>> createDate="+posts.getCreateDate()+", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
