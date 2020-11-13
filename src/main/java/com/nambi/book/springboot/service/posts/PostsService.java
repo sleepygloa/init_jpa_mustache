e9802ad21ccf9fc7ea32678ad3c480cfb09470d8
sleepygloa <sleepygloa@gmail.com>
2020-11-13 11:01:37 +0900

    1.글 리스트, 수정, 삭제 추가

--- src/main/java/com/nambi/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/nambi/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,20 @@ package com.nambi.book.springboot.service.posts;
 
 import com.nambi.book.springboot.domain.posts.Posts;
 import com.nambi.book.springboot.domain.posts.PostsRepository;
+import com.nambi.book.springboot.web.dto.PostsListResponseDto;
 import com.nambi.book.springboot.web.dto.PostsResponseDto;
 import com.nambi.book.springboot.web.dto.PostsSaveRequestDto;
 import com.nambi.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+
+import java.nio.ReadOnlyBufferException;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +43,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAlldesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(posts);
     }
 }
