a3a55163b3e1907e408a82fdff5d979fb6c91759
sleepygloa <sleepygloa@gmail.com>
2020-11-17 10:23:36 +0900

    네이버 로그인 연동 추가로 db세션으로 구글로그인, 네이버로그인 동일 세션으로 로그인되는 것을 확인

--- src/main/java/com/nambi/book/springboot/config/auth/OAuthAttributes.java
+++ src/main/java/com/nambi/book/springboot/config/auth/OAuthAttributes.java
@@ -27,9 +27,16 @@ public class OAuthAttributes {
 
     //OAuth2User에서 반환하는 사용자 정보는 Map 이기 때문에 값 하나하나 변환해야한다.
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        //네이버
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
+
+        //구글
         return ofGoogle(userNameAttributeName, attributes);
     }
 
+    //구글 로그인
     public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
         return OAuthAttributes.builder()
                 .name((String)attributes.get("name"))
@@ -38,7 +45,20 @@ public class OAuthAttributes {
                 .attribute(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
-        }
+    }
+
+    //네이버 로그인
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attribute){
+        Map<String, Object> response = (Map<String, Object>) attribute.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attribute(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
 
     //User 엔티티 생성
     //OAuthAttributes 에서 엔티티를 생성하는 시점은 처음 가입할때
