72cd0d1ab2deea6a244a75cf1e180d673f2891b6
sleepygloa <sleepygloa@gmail.com>
2020-11-17 09:42:13 +0900

    자주사용하는 session user 어노테이션으로 만들기

--- src/main/java/com/nambi/book/springboot/web/IndexController.java
+++ src/main/java/com/nambi/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.nambi.book.springboot.web;
 
+import com.nambi.book.springboot.config.auth.LoginUser;
 import com.nambi.book.springboot.config.auth.dto.SessionUser;
 import com.nambi.book.springboot.service.posts.PostsService;
 import com.nambi.book.springboot.web.dto.PostsResponseDto;
@@ -19,10 +20,10 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAlldesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
