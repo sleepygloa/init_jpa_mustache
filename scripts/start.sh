c3d8df9a15944750eb094c2be34f1a9275d157f0
sleepygloa <sleepygloa@gmail.com>
2020-11-20 15:46:27 +0900

    201120 nginx 무중단배포 sh 파일 오타수정3

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ PROJECT_NAME=com.nambi.book
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 새 애플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n -1)
@@ -27,6 +27,7 @@ IDLE_PROFILE=$(find_idle_profile)
 
 echo "> JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
+nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=$IDLE_PROFILE \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
