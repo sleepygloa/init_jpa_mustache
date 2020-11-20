2ff74180157c985f9a298179b37a877d45d3c233
sleepygloa <sleepygloa@gmail.com>
2020-11-20 16:22:49 +0900

    201120 nginx 무중단배포 sh 파일 오타수정6

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -14,6 +14,13 @@ function find_idle_profile()
     CURRENT_PROFILE=$(curl -s http://localhost/profile)
   fi
 
+  if [ ${CURRENT_PROFILE} == real1 ]
+  then
+    IDLE_PROFILE=real2
+  else
+    IDLE_PROFILE=real1
+  fi
+
   echo "${IDLE_PROFILE}"
 }
 
