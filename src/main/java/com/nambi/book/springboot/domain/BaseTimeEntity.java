630e9fe7d726f3145f0f9dbb5838f5de1962ef53
sleepygloa <sleepygloa@gmail.com>
2020-11-23 13:53:31 +0900

    create Date 오타 수정

--- src/main/java/com/nambi/book/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/nambi/book/springboot/domain/BaseTimeEntity.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    private LocalDateTime createDate;
+    private LocalDateTime createdDate;
 
     @LastModifiedDate
     private LocalDateTime modifiedDate;
