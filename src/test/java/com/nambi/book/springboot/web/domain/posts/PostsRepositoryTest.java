27ee0a4bf844d117b7c48b2f347d0140f780bb49
sleepygloa <sleepygloa@gmail.com>
2020-11-23 13:58:44 +0900

    create Date 오타 수정

--- src/test/java/com/nambi/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/nambi/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -66,9 +66,9 @@ public class PostsRepositoryTest {
         //then
         Posts posts = postsList.get(0);
 
-        System.out.println(">>>>>>>>>>>>>> createDate="+posts.getCreateDate()+", modifiedDate=" + posts.getModifiedDate());
+        System.out.println(">>>>>>>>>>>>>> createDate="+posts.getCreatedDate()+", modifiedDate=" + posts.getModifiedDate());
 
-        assertThat(posts.getCreateDate()).isAfter(now);
+        assertThat(posts.getCreatedDate()).isAfter(now);
         assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
